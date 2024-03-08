5509bb35de81c0af7fbb2a9c4504f86e2e131565
amaro silva <amarosilvasouza1@gmail.com>
2024-03-08 02:36:09 -0300

    criando e redenrizando as optionConfig

--- src/scripts/createRenderConfige.js
+++ src/scripts/createRenderConfige.js
@@ -7,19 +7,56 @@ const createConfig = () => {
     const modalConfig = document.querySelector(".modal_config")
     modalConfig.showModal()
 
-    const titleConfig = document.createElement("h2")
-    titleConfig.innerText = "Configuraçao"
-    titleConfig.classList = "titleConfig"
-
     const imgPelfil = document.querySelector("img")
     imgPelfil.src = img
     imgPelfil.classList = "imgPerfil pointer"
+    
+    const titleConfig = document.createElement("h2")
+    titleConfig.innerText = "Configuraçao"
+    titleConfig.classList = "titleConfig"
 
     const closeButton = document.createElement("p")
     closeButton.innerText = "X"
     closeButton.classList = "closeButton"
 
     renderHeaderConfig(titleConfig, imgPelfil, closeButton)
+
+    const ulListConfig = document.createElement("ul")
+    ulListConfig.classList = "ulCardConfigList"
+
+    const optionColor = document.createElement("li")
+    optionColor.classList = "cardConfig optionColor"
+    optionColor.innerText = "Cores"
+
+    const optionBio = document.createElement("li")
+    optionBio.classList = "cardConfig optionBio"
+    optionBio.innerText = "Bio"
+
+    const optionPreferences = document.createElement("li")
+    optionPreferences.classList = "cardConfig optionPreferences"
+    optionPreferences.innerText = "Preferençias"
+
+    const optionFonts = document.createElement("li")
+    optionFonts.classList = "cardConfig optionFonts"
+    optionFonts.innerText = "Fonts-Chats"
+
+    const optionSecurit = document.createElement("li")
+    optionSecurit.classList = "cardConfig optionSecurit"
+    optionSecurit.innerText = "Securanças E Notificaçoes"
+
+    const optionShortly = document.createElement("li")
+    optionShortly.classList = "cardConfig optionShotly"
+    optionShortly.innerText = "Em Breve"
+
+    const optionDarkList = document.createElement("li")
+    optionDarkList.classList = "cardConfig optionDarkList"
+    optionDarkList.innerText = "Lista Negra"
+
+    const optionHelp = document.createElement("li")
+    optionHelp.classList = "cardConfig optionHelp"
+    optionHelp.innerText = "Ajuda-Contato"
+
+    renderSectionConfig(ulListConfig, optionColor, optionBio, optionPreferences, optionFonts, optionSecurit, optionShortly, optionDarkList, optionHelp)
 
     verify = true
 }
@@ -32,6 +69,21 @@ const renderHeaderConfig = (titleConfig, imgPelfil, closeButton) => {
     divConfig.appendChild(closeButton)
 
     closeButtonConfig()
+}
+
+const renderSectionConfig = (ulListConfig, optionColor, optionBio, optionPreferences, optionFonts, optionSecurit, optionShortly, optionDarkList, optionHelp) => {
+    const sectionConfig = document.querySelector(".configConteiner")
+
+    ulListConfig.appendChild(optionColor)
+    ulListConfig.appendChild(optionBio)
+    ulListConfig.appendChild(optionPreferences)
+    ulListConfig.appendChild(optionFonts)
+    ulListConfig.appendChild(optionSecurit)
+    ulListConfig.appendChild(optionShortly)
+    ulListConfig.appendChild(optionDarkList)
+    ulListConfig.appendChild(optionHelp)
+    
+    sectionConfig.appendChild(ulListConfig)
 }
 
 const closeButtonConfig = () => {
