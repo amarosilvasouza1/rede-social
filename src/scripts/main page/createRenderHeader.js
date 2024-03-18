63a2e1914470ab6e8d245de2ed0f5130a0014559
amaro silva <amarosilvasouza1@gmail.com>
2024-03-17 22:35:19 -0300

    ajustando e recriando a funçao select opition

--- src/scripts/main page/createRenderHeader.js
+++ src/scripts/main page/createRenderHeader.js
@@ -4,12 +4,16 @@ const img = "./src/icons/perfil.png"
 const user = "Amaro_Silva"
 
 const imgButton = "./src/icons/imgbutton.svg"
+const imgCloseButton = "./src/icons/imgbuttonclose.svg"
 
 const renderButtonClickArrow = () => {
     const arrowButtonConteiner = document.querySelector('.header_cfg')
 
     const sectionButtonArrow = document.createElement("section")
     sectionButtonArrow.classList = "section_button_arrow"
+
+    const divSelectOpition = document.createElement("div")
+    divSelectOpition.classList = "div_select_opition"
         
     const divArrowButton = document.createElement("div")
     divArrowButton.classList = "arrow_button_div"
@@ -18,33 +22,81 @@ const renderButtonClickArrow = () => {
     arrowButton.classList = "arrow_button_header pointer open"
     arrowButton.src = imgButton
 
-    sectionButtonArrow.append(divArrowButton)
+    arrowButtonConteiner.append(sectionButtonArrow)
+    sectionButtonArrow.append(divSelectOpition, divArrowButton)
     divArrowButton.append(arrowButton)
-    arrowButtonConteiner.append(divArrowButton)
-
+    
     const clickArrow = () => {
         const buttonArrow = document.querySelector(".open")
         buttonArrow.addEventListener("click", () => {
-            const sectionConteinerSelect = document.querySelector(".section_conteiner_select")
-            sectionConteinerSelect.classList.remove("off")
-            const buttonArrow = document.querySelector(".arrow_button_header")
-            buttonArrow.classList.remove("open")
-            buttonArrow.classList.add("close")
 
-            const buttonArrowClose = document.querySelector(".close")
-            buttonArrowClose.addEventListener("click", () => {
-                const sectionConteinerSelect = document.querySelector(".section_conteiner_select")
-                sectionConteinerSelect.classList.add("off")
-                const buttonArrow = document.querySelector(".arrow_button_header")
-                buttonArrow.classList.remove("close")
-                buttonArrow.classList.add("open")
-                clickArrow()
-                return
-            })
-            return
+            const headerSelectConteiner = document.querySelector('.div_select_opition')
+
+            const sectionConteinerSelect = document.createElement("section")
+            sectionConteinerSelect.classList = "section_conteiner_select"
+            
+            const ulContainerSelect = document.createElement("ul")
+            ulContainerSelect.classList = "ul_container_select"
+            
+            const liCardSelectPrivate = document.createElement("li")
+            liCardSelectPrivate.classList = "card_select_Privete card_select pointer"
+            liCardSelectPrivate.innerText = "Chat Privado"
+            
+            const liCardSelectParty = document.createElement("li")
+            liCardSelectParty.classList = "card_select_party card_select pointer"
+            liCardSelectParty.innerText = "Festa"
+            
+            const liCardSelectCommunity = document.createElement("li")
+            liCardSelectCommunity.classList = "card_select_community card_select pointer"
+            liCardSelectCommunity.innerText = "Comunidade"
+            
+            const liCardSelectGallery = document.createElement("li")
+            liCardSelectGallery.classList = "card_select_gallery card_select pointer"
+            liCardSelectGallery.innerText = "Galeria"
+            
+            const liCardSelectMatch = document.createElement("li")
+            liCardSelectMatch.classList = "card_select_match card_select pointer"
+            liCardSelectMatch.innerText = "Match"
+            
+            const liCardSelectVipChat = document.createElement("li")
+            liCardSelectVipChat.classList = "card_select_vip-chat card_select pointer"
+            liCardSelectVipChat.innerText = "Chat Vip"
+            liCardSelectVipChat.innerText = "Vip-Chat"
+            
+            const liCardSelectActivity = document.createElement("li")
+            liCardSelectActivity.classList = "card_select_activity card_select pointer"
+            
+            liCardSelectActivity.innerText = "Atividade"
+            
+            const liCardSelectSuggestionsChat = document.createElement("li")
+            liCardSelectSuggestionsChat.classList = "card_select_Suggestions-chat card_select pointer"
+            liCardSelectSuggestionsChat.innerText = "Segestão"
+            
+            ulContainerSelect.append(liCardSelectPrivate, liCardSelectParty, liCardSelectCommunity, liCardSelectGallery, liCardSelectMatch, liCardSelectVipChat, liCardSelectActivity, liCardSelectSuggestionsChat)
+            sectionConteinerSelect.append(ulContainerSelect)
+            headerSelectConteiner.append(sectionConteinerSelect)
+
+            const divOpenArrowButton = document.querySelector(".arrow_button_div")
+            const openArrowButton = document.querySelector(".open")
+            divOpenArrowButton.removeChild(openArrowButton)
+
+            const divCloseArrowButton = document.querySelector(".arrow_button_div")
+            const closeArrowButtonImg = document.createElement("img")
+            closeArrowButtonImg.classList = "arrow_button_header pointer close"
+            closeArrowButtonImg.src = imgCloseButton
+
+            divCloseArrowButton.append(closeArrowButtonImg)
+
+            const closeArrowClick = document.querySelector(".close")
+            closeArrowClick.addEventListener("click", () => {
+                const arrowButtonConteiner = document.querySelector('.header_cfg')
+                const reset = document.querySelector(".section_button_arrow")
+                arrowButtonConteiner.removeChild(reset)
+                renderButtonClickArrow()
+            })    
         })
     }
-    clickArrow()
+    clickArrow() 
 }
 
 const createHeaderPage = () => {
@@ -71,59 +123,6 @@ const createHeaderPage = () => {
 
     renderHeaderPage(saveHeader, imgPerfilDiv, imgPerfilImg, namePerfilDiv, txtPerfilName, imgPerfilButton)
     verifyConfig()
-
-    const headerSelectConteiner = document.querySelector('.header_cfg')
-
-    const sectionConteinerSelect = document.createElement("section")
-    sectionConteinerSelect.classList = "section_conteiner_select off"
- 
-    const ulContainerSelect = document.createElement("ul")
-    ulContainerSelect.classList = "ul_container_select"
-
-    const liCardSelectPrivate = document.createElement("li")
-    liCardSelectPrivate.classList = "card_select_Privete card_select pointer"
-    liCardSelectPrivate.innerText = "Chat Privado"
-
-    const liCardSelectParty = document.createElement("li")
-    liCardSelectParty.classList = "card_select_party card_select pointer"
-    liCardSelectParty.innerText = "Festa"
-
-    const liCardSelectCommunity = document.createElement("li")
-    liCardSelectCommunity.classList = "card_select_community card_select pointer"
-    liCardSelectCommunity.innerText = "Comunidade"
-
-    const liCardSelectGallery = document.createElement("li")
-    liCardSelectGallery.classList = "card_select_gallery card_select pointer"
-    liCardSelectGallery.innerText = "Galeria"
-
-    const liCardSelectMatch = document.createElement("li")
-    liCardSelectMatch.classList = "card_select_match card_select pointer"
-    liCardSelectMatch.innerText = "Match"
-
-    const liCardSelectVipChat = document.createElement("li")
-    liCardSelectVipChat.classList = "card_select_vip-chat card_select pointer"
-    liCardSelectVipChat.innerText = "Chat Vip"
-    liCardSelectVipChat.innerText = "Vip-Chat"
-
-    const liCardSelectActivity = document.createElement("li")
-    liCardSelectActivity.classList = "card_select_activity card_select pointer"
-    
-    liCardSelectActivity.innerText = "Atividade"
-
-    const liCardSelectSuggestionsChat = document.createElement("li")
-    liCardSelectSuggestionsChat.classList = "card_select_Suggestions-chat card_select pointer"
-    liCardSelectSuggestionsChat.innerText = "Segestão"
-
-    RenderSelectOption(headerSelectConteiner, sectionConteinerSelect, ulContainerSelect, liCardSelectPrivate, liCardSelectParty, liCardSelectCommunity, liCardSelectGallery, liCardSelectMatch, liCardSelectVipChat, liCardSelectActivity, liCardSelectSuggestionsChat)
-
-    renderButtonClickArrow()
-}
-
-const RenderSelectOption = (headerSelectConteiner, sectionConteinerSelect, ulContainerSelect, liCardSelectPrivate, liCardSelectParty, liCardSelectCommunity, liCardSelectGallery, liCardSelectMatch, liCardSelectVipChat, liCardSelectActivity, liCardSelectSuggestionsChat) => {
-
-    ulContainerSelect.append(liCardSelectPrivate, liCardSelectParty, liCardSelectCommunity, liCardSelectGallery, liCardSelectMatch, liCardSelectVipChat, liCardSelectActivity, liCardSelectSuggestionsChat)
-    sectionConteinerSelect.append(ulContainerSelect)
-    headerSelectConteiner.append(sectionConteinerSelect)
 }
 
 const renderHeaderPage = (saveHeader, imgPerfilDiv, imgPerfilImg, namePerfilDiv, txtPerfilName, imgPerfilButton) => {
@@ -132,4 +131,7 @@ const renderHeaderPage = (saveHeader, imgPerfilDiv, imgPerfilImg, namePerfilDiv,
     imgPerfilDiv.append(namePerfilDiv, imgPerfilButton)
     saveHeader.append(imgPerfilDiv)
 }
+
 createHeaderPage()
+
+renderButtonClickArrow()
